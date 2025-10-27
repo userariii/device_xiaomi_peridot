73b2a62db3b1826377847b4f21b18033be1a12c2
NotBlazeee28 <p.devansh@proton.me>
2025-10-27 23:08:54 +0530

    peridot: Let's Go @The-Clover-Project
    Change-Id: I0da2ce78acc9423c98ee4a307150b86d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_peridot.mk
+    $(LOCAL_DIR)/clover_peridot.mk
--- clover_peridot.mk
+++ clover_peridot.mk
@@ -8,13 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common The Clover Project stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
 
-PRODUCT_NAME := lineage_peridot
+# Clover Stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
